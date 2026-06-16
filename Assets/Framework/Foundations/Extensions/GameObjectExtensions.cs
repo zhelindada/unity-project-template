098fed67708d7934f3cfd8bd7602850281517a8c
zhelindada <3628079095@qq.com>
2026-06-16 17:54:53 +0800

    Fix compile error

--- Assets/Framework/Foundations/Extensions/GameObjectExtensions.cs
+++ Assets/Framework/Foundations/Extensions/GameObjectExtensions.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class GameObjectExtensions{
 
-    public T GetOrAdd<T>(this GameObject obj) where T : Component{
+    public static T GetOrAdd<T>(this GameObject obj) where T : Component{
         if(obj.TryGetComponent<T>(out T t))
             return t;
         return obj.AddComponent<T>();
